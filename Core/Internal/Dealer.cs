0c1c14e215c8003577c39cbc27c11bf5437d3139
Hekku2 <hekku2@gmail.com>
2018-09-19 21:33:50 +0300

    Deck is now suffled before dealing.

--- Core/Internal/Dealer.cs
+++ Core/Internal/Dealer.cs
@@ -1,22 +1,41 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Core.Internal
 {
 	public static class Dealer
 	{
+		private static Random rng = new Random();
+
 		public static Hand[] Deal(Card[] cards, int playerCount)
 		{
-			// TODO Actually suffle cards
+			var copy = cards.ToList();
+			Shuffle(copy);
 
 			var hands = new Hand[playerCount];
-			var cardCount = cards.Count() / playerCount;
+			var cardCount = copy.Count() / playerCount;
 			for (int i = 0; i < playerCount; i++)
 			{
-				var chunk = cards.Skip(i*cardCount).Take(cardCount);
+				var chunk = copy.Skip(i*cardCount).Take(cardCount);
 				var playerHand = new Hand(chunk);
 				hands[i] = playerHand;
 			}
 			return hands;
+		}
+
+		private static void Shuffle<T>(IList<T> list)
+		{
+			int n = list.Count;
+			
+			while (n > 1)
+			{
+				n--;
+				int k = rng.Next(n + 1);
+				T value = list[k];
+				list[k] = list[n];
+				list[n] = value;
+			}
 		}
 	}
 }
