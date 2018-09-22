87deb8e94192761cd05641d56f5f67807b0810d8
Hekku2 <hekku2@gmail.com>
2018-09-22 22:47:20 +0300

    Added more tests for Ai extensions methods

--- AiCandidatesTests/ExtensionsTests.cs
+++ AiCandidatesTests/ExtensionsTests.cs
@@ -20,6 +20,58 @@ namespace AiCandidatesTests
         }
 
         [Test]
+        public void LowestSafeCard_ReturnsLowestSafeCard()
+        {
+            var cards = new Card[]
+            {
+                new Card(CardType.Blue, 3, 1),
+                new Card(CardType.Blue, 2, 1),
+                new Card(CardType.Blue, 6, 1)
+            };
+            var lowest = cards.LowestSafeCard(2);
+            Assert.AreEqual(cards[0].Number, lowest.Number);
+        }
+
+        [Test]
+        public void LowestSafeCard_ReturnsNullIfThereAreNoSafeCards()
+        {
+            var cards = new Card[]
+            {
+                new Card(CardType.Blue, 3, 1),
+                new Card(CardType.Blue, 2, 1),
+                new Card(CardType.Blue, 6, 1)
+            };
+            var lowest = cards.LowestSafeCard(7);
+            Assert.IsNull(lowest);
+        }
+
+        [Test]
+        public void LowestWinningCard_ReturnsLowestWinningCard()
+        {
+            var cards = new Card[]
+            {
+                new Card(CardType.Blue, 2, 1),
+                new Card(CardType.Blue, 3, 1),
+                new Card(CardType.Blue, 6, 1)
+            };
+            var lowest = cards.LowestWinningCard(2);
+            Assert.AreEqual(cards[1].Number, lowest.Number);
+        }
+
+        [Test]
+        public void LowestLowestWinningCard_ReturnsNullIfThereAreNoWinningCards()
+        {
+            var cards = new Card[]
+            {
+                new Card(CardType.Blue, 3, 1),
+                new Card(CardType.Blue, 2, 1),
+                new Card(CardType.Blue, 6, 1)
+            };
+            var lowest = cards.LowestWinningCard(7);
+            Assert.IsNull(lowest);
+        }
+
+        [Test]
         public void HighestCard_ReturnsCardWithLowestValue()
         {
             var cards = new Card[]
