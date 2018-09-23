365d6bb20894f96abd31c1ee7f980f677e8497f3
Hekku2 <hekku2@gmail.com>
2018-09-23 18:18:26 +0300

    Added sanity checks for FirstCandidate and added more tests for TrickEngine

---
+++ AiCandidatesTests/FirstCandidateTests.cs
@@ -0,0 +1,91 @@
+using AiCandidates;
+using Core;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using NUnit.Framework;
+
+[TestFixture]
+public class FirstCandidateTests
+{
+
+    private FirstCandidate _ai;
+
+    [SetUp]
+    public void Setup()
+    {
+        var mockLogger = Substitute.For<ILogger<FirstCandidate>>();
+        _ai = new FirstCandidate(mockLogger, new Stats());
+    }
+
+    [Test]
+    public void Play_ReturnsWinningCardForClearWin()
+    {
+        _ai.GameStart(3, 1, 0);
+
+        var playerHand = new []
+        {
+            new Card(CardType.Blue, 1, 1), // left
+            new Card(CardType.Blue, 2, 1), // right
+            new Card(CardType.Blue, 3, 1), // bottle
+            new Card(CardType.Blue, 36, 1)
+        };
+        var initial = _ai.RoundStart(playerHand);
+        _ai.ReceivedCards(playerHand[1], playerHand[2]);
+
+        var currentPlay = new []
+        {
+            new Card(CardType.Blue, 34, 1),
+            new Card(CardType.Blue, 33, 1)
+        };
+        var result = _ai.Play(currentPlay);
+        Assert.AreEqual(playerHand[3], result, "Only one winning option");
+    }
+
+    [Test]
+    public void Play_ShouldDiscardLowCardWhenPossible()
+    {
+        _ai.GameStart(3, 1, 0);
+
+        var playerHand = new []
+        {
+            new Card(CardType.Blue, 1, 1), // left
+            new Card(CardType.Blue, 2, 1), // right
+            new Card(CardType.Blue, 20, 1), // bottle
+            new Card(CardType.Blue, 36, 1)
+        };
+        var initial = _ai.RoundStart(playerHand);
+        _ai.ReceivedCards(playerHand[1], playerHand[2]);
+
+        var currentPlay = new []
+        {
+            new Card(CardType.Blue, 4, 1),
+            new Card(CardType.Blue, 33, 1)
+        };
+        var result = _ai.Play(currentPlay);
+        Assert.AreEqual(playerHand[1], result, "Play should be low card because it's possible");
+    }
+
+	[Test]
+	public void Play_ShouldDiscardLowestSafeCardWhenPossible()
+	{
+		_ai.GameStart(3, 1, 0);
+
+		var playerHand = new[]
+		{
+			new Card(CardType.Blue, 1, 1), // left
+            new Card(CardType.Blue, 2, 1), // right
+            new Card(CardType.Blue, 20, 1), // bottle
+			new Card(CardType.Blue, 25, 1)
+		};
+		var initial = _ai.RoundStart(playerHand);
+		_ai.ReceivedCards(playerHand[1], playerHand[2]);
+
+		var currentPlay = new[]
+		{
+			new Card(CardType.Blue, 21, 1),
+			new Card(CardType.Blue, 37, 1)
+		};
+		var result = _ai.Play(currentPlay);
+		Assert.AreEqual(playerHand[2], result, "Play should be low card because it's possible");
+	}
+}
--- CoreTests/Internal/TrickEngineTests.cs
+++ CoreTests/Internal/TrickEngineTests.cs
@@ -7,6 +7,8 @@ using NUnit.Framework;
 [TestFixture]
 public class TrickEngineTests
 {
+	private const int StartingBottlePrice = 19; 
+
     [Test]
     public void PlayTrick_StartsFromFirstPlayerIfThereIsNoPreviousResult()
     {
@@ -26,4 +28,22 @@ public class TrickEngineTests
         Assert.AreEqual(seats[2], result.Winner);
         Assert.AreEqual(3, result.WinnerPrice().Count());
     }
+
+	[Test]
+	public void PlayTrick_SetsCorrectWinnerWhenNoCardIsUnderBottlePrice()
+	{
+		var seats = Enumerable.Range(0, 3).Select(index => {
+			var card = new Card(CardType.Blue, index + StartingBottlePrice + 1, 1);
+			var hand = new Hand(new[] { card });
+			var player = Substitute.For<IGameAi>();
+			player.Play(Arg.Any<Card[]>()).Returns(card);
+			var seat = new Seat(index, player, hand);
+			return seat;
+		}).ToArray();
+		var result = TrickEngine.PlayTrick(null, seats);
+		Assert.IsNull(result.BottleOwner);
+		Assert.AreEqual(StartingBottlePrice, result.BottlePrice);
+		Assert.AreEqual(seats[2], result.Winner);
+		Assert.AreEqual(3, result.WinnerPrice().Count());
+	}
 }
