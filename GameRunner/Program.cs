acc619b23aabdc89df81c3765083557890ce6a66
Hekku2 <hekku2@gmail.com>
2018-09-23 18:19:01 +0300

    Program.cs indentation

--- GameRunner/Program.cs
+++ GameRunner/Program.cs
@@ -7,44 +7,44 @@ using System.Linq;
 
 namespace GameRunner
 {
-	public sealed class Program
-	{
-		public static void Main(string[] args)
-		{
-			var serviceCollection = new ServiceCollection();
-			ConfigureServices(serviceCollection);
-			var serviceProvider = serviceCollection.BuildServiceProvider();
-			var logger = serviceProvider.GetService<ILogger<FirstCandidate>>();
+    public sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var logger = serviceProvider.GetService<ILogger<FirstCandidate>>();
 
-			var stats = new Stats();
+            var stats = new Stats();
 
-			const int totalRounds = 1;
-			var ais = new IGameAi[]
-			{
-				new FirstCandidate(logger, stats),
-				new AiCandidates.Random(),
-				new AiCandidates.Random(),
-				new AiCandidates.Random(),
-			};
-			var engine = new GameEngine(totalRounds, ais);
-			var result = engine.Run();
+            const int totalRounds = 200;
+            var ais = new IGameAi[]
+            {
+                new FirstCandidate(logger, stats),
+                new AiCandidates.Random(),
+                new AiCandidates.Random(),
+                new AiCandidates.Random(),
+            };
+            var engine = new GameEngine(totalRounds, ais);
+            var result = engine.Run();
 
-			var total = result.Sum();
-			var percents = result.Select(r => Percent(total, r)).ToList();
+            var total = result.Sum();
+            var percents = result.Select(r => Percent(total, r)).ToList();
 
-			Console.WriteLine($"Result: {percents[0]:0.00}%, {percents[1]:0.00}%, {percents[2]:0.00}%, {percents[3]:0.00}%");
-			Console.WriteLine($"Result: {result[0]}, {result[1]}, {result[2]}, {result[3]}");
-			Console.WriteLine($"Round won {stats.RoundWins}, lost {stats.RoundLoses}, undecided paths {stats.UndecidedPath}");
-		}
+            Console.WriteLine($"Result: {percents[0]:0.00}%, {percents[1]:0.00}%, {percents[2]:0.00}%, {percents[3]:0.00}%");
+            Console.WriteLine($"Result: {result[0]}, {result[1]}, {result[2]}, {result[3]}");
+            Console.WriteLine($"Round won {stats.RoundWins}, lost {stats.RoundLoses}, undecided paths {stats.UndecidedPath}");
+        }
 
-		private static void ConfigureServices(IServiceCollection services)
-		{
-			services.AddLogging(configure => configure.AddConsole());
-		}
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddConsole());
+        }
 
-		private static double Percent(int total, int score)
-		{
-			return (score / (total*1.0)) * 100;
-		}
-	}
+        private static double Percent(int total, int score)
+        {
+            return (score / (total * 1.0)) * 100;
+        }
+    }
 }
